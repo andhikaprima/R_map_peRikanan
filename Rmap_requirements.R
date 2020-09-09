f393973329e10276f70951dd6138d0e9fe618829
Andhika Prasetyo <dhika_fishery@yahoo.com>
2020-09-09 17:21:12 +0100

    Add files via upload

---
+++ Rmap_requirements.R
@@ -0,0 +1,17 @@
+install.packages("sp")     
+install.packages("raster")
+install.packages("maps")  
+install.packages("mapdata") 
+install.packages("ggplot2") 
+install.packages("ggthemes") 
+install.packages("ggrepel") 
+install.packages("geosphere")
+install.packages("readxl") 
+install.packages("dplyr")
+install.packages("viridis")
+install.packages("plotly")
+install.packages("devtools")
+devtools::install_github("rasyidstat/indonesia")
+
+
+
